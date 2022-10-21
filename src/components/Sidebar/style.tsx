d124bd47ca179f97b98ce9840b28fe61a69520c9
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-21 14:02:53 +0200

    style: styling side bar

---
+++ src/components/Sidebar/style.tsx
@@ -0,0 +1,55 @@
+import styled from "@emotion/styled";
+import { theme } from "../../styles/theme";
+
+export const SideBar = styled.div`
+  width: 350px;
+  border-right: 1px solid ${theme.Border};
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    border-right: none;
+  }
+`;
+
+export const Navbar = styled.div`
+  display: flex;
+  padding: 15px 30px;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${theme.Border};
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+`;
+
+export const Logo = styled.div`
+  font-weight: 500;
+  color: ${theme.Black};
+  font-size: calc(20 / 16 * 1rem);
+`;
+
+export const ProfileButton = styled.button`
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
+`;
+
+export const ChatButton = styled.button`
+  width: 40px;
+  height: 40px;
+  border: none;
+  display: flex;
+  cursor: pointer;
+  margin-right: 20px;
+  border-radius: 50%;
+  align-items: center;
+  color: ${theme.Black};
+  justify-content: center;
+  background-color: ${theme.Border};
+`;
+
+export const ProfilePicture = styled.img`
+  width: 40px;
+  border-radius: 50%;
+`;
