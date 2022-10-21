b53cbed41729de818ffafcb8a9b22c40c6fe8eac
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-21 09:58:05 +0200

    feat: implementing react router, private route, listen if user ias authenticated

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,61 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { setDoc, doc } from "firebase/firestore";
+import { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
+import { Spinner, PrivateRoute } from "./components";
+import { firebaseAuth, firebaseDb, useUserStore } from "./library/index";
+import { Chat, Home, SignIn } from "./pages";
+
 export function App() {
-  return <div>App</div>;
+  const currentUser = useUserStore((state) => state.currentUser);
+  const setCurrentUser = useUserStore((state) => state.setCurrentUser);
+
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      if (user) {
+        setCurrentUser(user);
+        setDoc(doc(firebaseDb, `users/${user.uid}`), {
+          uid: user.uid,
+          email: user.email,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          phoneNumber: user.phoneNumber || user.providerData?.[0]?.phoneNumber,
+        });
+      } else setCurrentUser(null);
+    });
+  }, []);
+
+  if (typeof currentUser === "undefined")
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <Spinner />
+      </div>
+    );
+
+  return (
+    <>
+      <Routes>
+        <Route
+          index
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="/sign-in" element={<SignIn />} />
+
+        <Route
+          path=":id"
+          element={
+            <PrivateRoute>
+              <Chat />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
 }
