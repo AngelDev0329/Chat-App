a48b0ab42120fd62d15fc2725bc1f53f6573115e
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-23 17:42:04 +0200

    refactor: import right name

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,8 @@
 export const theme = {
-  White: "#fff",
-  Black: "#24292f",
-  Border: "#e2e8ef",
-  LightGrey: "#6b7280",
-  DarkGrey: "#4b5563",
-  Blue: "#4285F4",
-};
+  White: '#fff',
+  Black: '#24292f',
+  Border: '#e2e8ef',
+  LightGrey: '#6b7280',
+  DarkGrey: '#4b5563',
+  Blue: '#4285F4',
+}
