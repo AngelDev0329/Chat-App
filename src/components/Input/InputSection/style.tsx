acceb2d858705f816ab47155be39bb517ec8cea4
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-23 09:59:16 +0200

    style:  Drag and drop: background styling

--- src/components/Input/InputSection/style.tsx
+++ src/components/Input/InputSection/style.tsx
@@ -3,9 +3,9 @@ import styled from '@emotion/styled'
 import { theme } from '../../../styles/theme'
 
 export const Container = styled.div`
+  display: flex;
   margin-top: 10px;
   padding: 15px 20px;
-  display: flex;
   align-items: center;
   justify-content: center;
   border-top: 1px solid ${theme.Border};
@@ -58,22 +58,33 @@ export const Input = styled.input`
   }
 `
 
-export const EmojiButton = styled.button`
-  top: 5px;
-  right: 10px;
-  border: none;
-  font-size: 1.3rem;
-  position: absolute;
-  color: ${theme.DarkGrey};
-  background-color: transparent;
-`
-
 export const SendButton = styled.button`
   border: none;
   display: flex;
-  padding-left: 10px;
   font-size: 1.6rem;
+  padding-left: 10px;
   align-items: center;
   background: transparent;
   color: ${theme.DarkGrey};
+`
+
+export const DragFile = styled.div`
+  z-index: 20;
+  width: 100vh;
+  display: flex;
+  height: 100vh;
+  position: fixed;
+  user-select: none;
+  align-items: center;
+  pointer-events: none;
+  justify-content: center;
+  transition: all 0.3s ease;
+
+  background-color: #3333331b;
+`
+
+export const Title = styled.div`
+  z-index: 30;
+  color: ${theme.Black};
+  font-size: calc(30 / 16 * 1rem);
 `
