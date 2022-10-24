0cc83c7479a5397aab46a17212d4e7f6cd891dd5
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-24 14:08:24 +0200

    change to url

--- src/library/constant.ts
+++ src/library/constant.ts
@@ -1,40 +1,41 @@
-export const DEFAULT_AVATAR = "/default-avatar.png";
+export const DEFAULT_AVATAR =
+  'https://woodfibreinsulation.co.uk/wp-content/uploads/2017/04/blank-profile-picture-973460.png'
 
 export const IMAGE_PROXY = (url: string) =>
-  `https://apoqrsgtqq.cloudimg.io/${url}`;
+  `https://apoqrsgtqq.cloudimg.io/${url}`
 
 export const REACTIONS_UI: {
   [key: string]: {
-    icon: string;
-    gif: string;
-  };
+    icon: string
+    gif: string
+  }
 } = {
   Like: {
-    icon: "/public/reactions-icon/like.svg",
-    gif: "/public/reactions/like.gif",
+    icon: '/public/reactions-icon/like.svg',
+    gif: '/public/reactions/like.gif',
   },
   Love: {
-    icon: "/public/reactions-icon/love.svg",
-    gif: "/public/reactions/love.gif",
+    icon: '/public/reactions-icon/love.svg',
+    gif: '/public/reactions/love.gif',
   },
   Care: {
-    icon: "/public/reactions-icon/care.svg",
-    gif: "/public/reactions/care.gif",
+    icon: '/public/reactions-icon/care.svg',
+    gif: '/public/reactions/care.gif',
   },
   Haha: {
-    icon: "/public/reactions-icon/haha.svg",
-    gif: "/public/reactions/haha.gif",
+    icon: '/public/reactions-icon/haha.svg',
+    gif: '/public/reactions/haha.gif',
   },
   Wow: {
-    icon: "/public/reactions-icon/wow.svg",
-    gif: "/public/reactions/wow.gif",
+    icon: '/public/reactions-icon/wow.svg',
+    gif: '/public/reactions/wow.gif',
   },
   Sad: {
-    icon: "/public/reactions-icon/sad.svg",
-    gif: "/public/reactions/sad.gif",
+    icon: '/public/reactions-icon/sad.svg',
+    gif: '/public/reactions/sad.gif',
   },
   Angry: {
-    icon: "/public/reactions-icon/angry.svg",
-    gif: "/public/reactions/angry.gif",
+    icon: '/public/reactions-icon/angry.svg',
+    gif: '/public/reactions/angry.gif',
   },
-};
+}
