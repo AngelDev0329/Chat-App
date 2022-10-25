53e959c7a7a84f99fc1a5c11de43c3d19c771088
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-25 17:17:00 +0200

    feat: ChatView system done, (window scroll, fetching messages,  creating query collection {index},)

--- src/components/Chat/ChatView/ChatView.tsx
+++ src/components/Chat/ChatView/ChatView.tsx
@@ -1,12 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { ConversationInfo } from '../../../library'
 
-import { limitToLast, orderBy, query, collection } from 'firebase/firestore'
-import { useState } from 'react'
+import {
+  limitToLast,
+  orderBy,
+  query,
+  collection,
+  updateDoc,
+  doc,
+} from 'firebase/firestore'
+import { useEffect, useRef, useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { useParams } from 'react-router-dom'
 
 import { useCollectionQuery } from '../../../hooks'
+import { useUserStore } from '../../../library'
 import { firebaseDb } from '../../../library'
 import { Spinner } from '../../Spinner/Spinner'
 
@@ -22,6 +30,79 @@ export function ChatView({
   setReplyInfo,
   inputSectionOffset,
 }: ChatViewProps) {
+  const { id: conversationId } = useParams()
+  const currentUser = useUserStore((state) => state.currentUser)
+  const scrollBottomRef = useRef<HTMLDivElement>(null)
+
+  const [limitCount, setLimitCount] = useState(10)
+
+  const { data, loading, error } = useCollectionQuery(
+    `conversation-data-${conversationId}-${limitCount}`,
+    query(
+      collection(
+        firebaseDb,
+        'conversations',
+        conversationId as string,
+        'message'
+      ),
+      orderBy('createdAt'),
+      limitToLast(limitCount)
+    )
+  )
+
+  const dataRef = useRef(data)
+  const conversationIdRef = useRef(conversationId)
+  const isWindowFocus = useRef(true)
+
+  const updateSeenStatus = () => {
+    if (dataRef.current?.empty) return
+
+    const lastDoc = dataRef.current?.docs?.slice(-1)?.[0]
+
+    if (!lastDoc) return
+
+    updateDoc(
+      doc(firebaseDb, 'conversations', conversationIdRef.current as string),
+      {
+        [`seen.${currentUser?.uid}`]: lastDoc.id,
+      }
+    )
+  }
+
+  useEffect(() => {
+    dataRef.current = data
+  }, [data])
+
+  useEffect(() => {
+    conversationIdRef.current = conversationId
+  }, [conversationId])
+
+  useEffect(() => {
+    if (isWindowFocus.current) updateSeenStatus()
+    scrollBottomRef.current?.scrollIntoView()
+    setTimeout(() => {
+      scrollBottomRef.current?.scrollIntoView()
+    }, 100)
+  }, [data?.docs?.slice(-1)?.[0]?.id || ''])
+
+  useEffect(() => {
+    const focusHandler = () => {
+      isWindowFocus.current = true
+      updateSeenStatus()
+    }
+
+    const blurHandler = () => {
+      isWindowFocus.current = false
+    }
+    addEventListener('focus', focusHandler)
+    addEventListener('blur', blurHandler)
+
+    return () => {
+      removeEventListener('focus', focusHandler)
+      removeEventListener('blur', blurHandler)
+    }
+  }, [])
+
   return (
     <div
       style={{ height: `calc(100vh - ${144 + inputSectionOffset}px)` }}
