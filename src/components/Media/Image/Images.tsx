df9c65cbef400d23fe33c24cd8befb5c7ac9586b
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-25 16:58:58 +0200

    change Image layout

--- src/components/Media/Image/Images.tsx
+++ src/components/Media/Image/Images.tsx
@@ -1,36 +1,12 @@
 import { collection, query, where, orderBy } from 'firebase/firestore'
-import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { useCollectionQuery } from '../../../hooks'
 import { firebaseDb } from '../../../library'
 import { MiniSpinner } from '../../MiniSpinner/MiniSpinner'
-import ImageView from './ImageView'
+import { Info, Image, Grid } from '../style'
 
-type T = {
-  src: string
-}
-function ImageItem({ src }: T) {
-  const [isImageViewOpen, setIsImageViewOpen] = useState(false)
-
-  return (
-    <>
-      <img
-        onClick={() => setIsImageViewOpen(true)}
-        className="h-[100px] w-[100px] cursor-pointer object-cover transition duration-300 hover:brightness-75"
-        src={src}
-        alt=""
-      />
-      <ImageView
-        src={src}
-        isOpened={isImageViewOpen}
-        setIsOpened={setIsImageViewOpen}
-      />
-    </>
-  )
-}
-
-const Image = () => {
+export function ImageItem() {
   const { id: conversationId } = useParams()
 
   const { data, loading, error } = useCollectionQuery(
@@ -54,20 +30,13 @@ const Image = () => {
       </div>
     )
 
-  if (data?.empty)
-    return (
-      <div className="h-80 py-3">
-        <p className="text-center">No image found</p>
-      </div>
-    )
+  if (data?.empty) return <Info className="text-center">No image found</Info>
 
   return (
-    <div className="flex h-80 flex-wrap content-start gap-4 overflow-y-auto overflow-x-hidden p-4">
+    <Grid>
       {data?.docs.map((image) => (
-        <ImageItem key={image.id} src={image.data().content} />
+        <Image key={image.id} src={image.data().content} />
       ))}
-    </div>
+    </Grid>
   )
 }
-
-export default Image
