41af84ace0fa6fa3cb10c25c3c3b621215e43cb4
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-25 12:01:50 +0200

    style: add styling to conversation setting

--- src/components/Chat/ConversationSettings/ConversationSettings.tsx
+++ src/components/Chat/ConversationSettings/ConversationSettings.tsx
@@ -7,12 +7,12 @@ import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
 import { useRef, useState } from 'react'
 import {
   FiChevronDown,
-  FiChevronUp,
-  FiEdit3,
+  FiEdit,
   FiFile,
-  FiImage,
   FiLogOut,
   FiX,
+  FiCamera,
+  FiChevronRight,
 } from 'react-icons/fi'
 import { useNavigate, useParams } from 'react-router-dom'
 
@@ -20,6 +20,16 @@ import { firebaseStorage, formatFileName } from '../../../library'
 import { firebaseDb } from '../../../library'
 import { useUserStore } from '../../../library'
 import { AlertMessage } from '../../Alert'
+import {
+  CloseButton,
+  Header,
+  Title,
+  Container,
+  Button,
+  Form,
+  NameInput,
+  NameButton,
+} from './styled'
 
 type ConversationSettingsProps = {
   isOpen: boolean
@@ -109,83 +119,75 @@ export function ConversationSettings({
   }
   return (
     <Dialog onClose={handleClose} open={isOpen}>
-      <div
-        onClick={() => setIsOpen(false)}
-        className={`animate-fade-in fixed top-0 left-0 z-20 flex h-full w-full items-center justify-center bg-[#00000080] transition-all duration-300`}
-      >
-        <div
-          onClick={(e) => e.stopPropagation()}
-          className="bg-dark mx-2 w-full max-w-[500px] rounded-lg"
+      <Header>
+        <Title>Conversation settings</Title>
+        <CloseButton onClick={() => setIsOpen(false)}>
+          <FiX />
+        </CloseButton>
+      </Header>
+
+      <Container>
+        {conversation.users.length > 2 && (
+          <>
+            <Button onClick={() => setIsChangeChatNameOpen((prev) => !prev)}>
+              <FiEdit style={{ fontSize: '1.3rem', marginRight: '10px' }} />{' '}
+              Change chat name
+              {isChangeChatNameOpen ? (
+                <FiChevronDown style={{ marginLeft: 'auto' }} />
+              ) : (
+                <FiChevronRight style={{ marginLeft: 'auto' }} />
+              )}
+            </Button>
+            {isChangeChatNameOpen && (
+              <Form onSubmit={handleFormSubmit}>
+                <NameInput
+                  value={chatNameInputValue}
+                  onChange={(e) => setChatNameInputValue(e.target.value)}
+                  type="text"
+                  placeholder="Chat name"
+                />
+                <NameButton>Change</NameButton>
+              </Form>
+            )}
+            <Button onClick={() => fileInputRef.current?.click()}>
+              <FiCamera style={{ fontSize: '1.3rem', marginRight: '10px' }} />
+              Change group photo
+            </Button>
+
+            <input
+              hidden
+              ref={fileInputRef}
+              type="file"
+              accept="image/*"
+              onChange={handleFileInputChange}
+            />
+
+            <AlertMessage
+              isOpen={isAlertOpen}
+              setIsOpen={setIsAlertOpen}
+              text={alertText}
+              isError
+            />
+          </>
+        )}
+
+        <Button
+          onClick={() => {
+            setIsOpen(false)
+            setMediaViewOpen(true)
+          }}
         >
-          <div className="border-dark-lighten flex items-center justify-between border-b py-3 px-3">
-            <h1>Conversation settings</h1>
-            <button onClick={() => setIsOpen(false)}>
-              <FiX />
-            </button>
-          </div>
+          <FiFile style={{ fontSize: '1.3rem', marginRight: '10px' }} /> View
+          images & files
+        </Button>
 
-          <div>
-            {conversation.users.length > 2 && (
-              <>
-                <button
-                  onClick={() => setIsChangeChatNameOpen((prev) => !prev)}
-                >
-                  <div>
-                    <FiEdit3 /> Change chat name
-                  </div>
-
-                  {isChangeChatNameOpen ? <FiChevronUp /> : <FiChevronDown />}
-                </button>
-                {isChangeChatNameOpen && (
-                  <form onSubmit={handleFormSubmit}>
-                    <input
-                      value={chatNameInputValue}
-                      onChange={(e) => setChatNameInputValue(e.target.value)}
-                      type="text"
-                      placeholder="Chat name"
-                    />
-                    <button>Change</button>
-                  </form>
-                )}
-                <button onClick={() => fileInputRef.current?.click()}>
-                  <FiImage />
-                  <span>Change group photo</span>
-                </button>
-
-                <input
-                  hidden
-                  ref={fileInputRef}
-                  type="file"
-                  accept="image/*"
-                  onChange={handleFileInputChange}
-                />
-
-                <AlertMessage
-                  isOpen={isAlertOpen}
-                  setIsOpen={setIsAlertOpen}
-                  text={alertText}
-                  isError
-                />
-              </>
-            )}
-
-            <button
-              onClick={() => {
-                setIsOpen(false)
-                setMediaViewOpen(true)
-              }}
-            >
-              <FiFile /> View images & files
-            </button>
-
-            {conversation.users.length > 2 && (
-              <button onClick={() => leaveGroup()}>
-                <FiLogOut /> Leave group
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
+        {conversation.users.length > 2 && (
+          <Button onClick={() => leaveGroup()}>
+            <FiLogOut style={{ fontSize: '1.25rem', marginRight: '10px' }} />{' '}
+            Leave group
+          </Button>
+        )}
+      </Container>
     </Dialog>
   )
 }
