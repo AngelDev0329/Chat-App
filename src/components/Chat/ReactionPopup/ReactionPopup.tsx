059f99c4580532f8360871d65efa129200db884d
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-26 13:43:48 +0200

    feat: create reaction pop up

--- src/components/Chat/ReactionPopup/ReactionPopup.tsx
+++ src/components/Chat/ReactionPopup/ReactionPopup.tsx
@@ -1,8 +1,10 @@
-import type { Ref } from 'react'
+import { updateDoc, doc } from 'firebase/firestore'
+import { useParams } from 'react-router-dom'
+
+import { firebaseDb, REACTIONS_UI, useUserStore } from '../../../library'
 
 type ReactionPopupProps = {
   position: 'left' | 'right'
-  forwardedRef: Ref<HTMLDivElement>
   setIsOpen: (value: boolean) => void
   messageId: string
   currentReaction: number
@@ -10,10 +12,48 @@ type ReactionPopupProps = {
 
 export function ReactionPopup({
   position,
-  forwardedRef,
   setIsOpen,
   messageId,
   currentReaction,
 }: ReactionPopupProps) {
-  return <div>ReactionPopup</div>
+  const { id: conversationId } = useParams()
+
+  const currentUser = useUserStore((state) => state.currentUser)
+
+  const updateReaction = (value: number) => {
+    updateDoc(
+      doc(
+        firebaseDb,
+        'conversations',
+        conversationId as string,
+        'messages',
+        messageId
+      ),
+      {
+        [`reactions.${currentUser?.uid}`]: value,
+      }
+    )
+  }
+
+  return (
+    <div className={position === 'left' ? 'left-8' : 'right-8'}>
+      {Object.entries(REACTIONS_UI).map(([key, value], index) => (
+        <div
+          key={key}
+          className={index + 1 === currentReaction ? 'current-reaction' : ''}
+        >
+          <img
+            onClick={() => {
+              if (index + 1 === currentReaction) updateReaction(0)
+              else updateReaction(index + 1)
+              setIsOpen(false)
+            }}
+            title={key}
+            src={value.gif}
+            alt=""
+          />
+        </div>
+      ))}
+    </div>
+  )
 }
