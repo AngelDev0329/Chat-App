b5247979745ec2f0ee27730157b4aa59253bd9fe
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-10-27 08:30:51 +0200

    style:  more small styling

--- src/components/Sidebar/Profile/style.tsx
+++ src/components/Sidebar/Profile/style.tsx
@@ -7,6 +7,10 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  @media screen and (max-width: 565px) {
+    flex-direction: column;
+    text-align: center;
+  }
 `
 
 export const Wrapper = styled.div`
@@ -32,6 +36,7 @@ export const CloseButton = styled.button`
 
 export const Image = styled.img`
   border-radius: 50%;
+  width: 80px;
 `
 
 export const Thick = styled.span`
@@ -40,15 +45,20 @@ export const Thick = styled.span`
 `
 
 export const Text = styled.p`
-  font-size: 1rem;
   line-height: 2;
+  @media screen and (max-width: 565px) {
+    font-size: 0.9rem;
+  }
 `
 
 export const Info = styled.p`
   margin: 0 auto;
-  padding-bottom: 20px;
+  padding: 0 20px 20px;
   text-align: center;
   color: ${theme.LightGrey};
   font-size: 0.9rem;
-  width: 500px;
+  width: 100%;
+  @media screen and (max-width: 565px) {
+    font-size: 0.9rem;
+  }
 `
